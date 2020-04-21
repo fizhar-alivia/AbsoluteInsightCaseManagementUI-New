109eb8bf42d5b65d5d7d8d26053008bc6128c281
Hamid <asheikh@aliviaanalytics.com>
2020-04-21 14:14:29 +0500

    Page not found component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthGuard } from './modules/auth/_guards/auth.guard';
 import { BaseComponent } from './modules/base/base.component';
-
+import { PageNotFoundComponent } from "./modules/base/common-components/error-pages/page-not-found/page-not-found.component";
 
 const routes: Routes = [
 
@@ -20,6 +20,19 @@ const routes: Routes = [
       // { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
     ]
   },
+
+  {
+    path: 'error/404',
+    component: PageNotFoundComponent,
+    data: {
+      type: 'error-v6',
+      code: 404,
+      title: '404... Page Not Found',
+      desc: 'Looks like you have entered a wrong url',
+    },
+  },
+  
+  {path: '**', redirectTo: 'error/404', pathMatch: 'full'},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BaseComponent } from './modules/base/base.component';
 import { SideNavComponent } from './modules/base/common-components/side-nav/side-nav.component';
 import { HeaderComponent } from './modules/base/common-components/header/header.component';
+import { PageNotFoundComponent } from './modules/base/common-components/error-pages/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BaseComponent,
     SideNavComponent,
-    HeaderComponent
+    HeaderComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
