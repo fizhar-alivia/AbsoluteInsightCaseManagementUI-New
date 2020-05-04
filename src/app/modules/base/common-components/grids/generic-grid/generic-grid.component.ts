39f92c61955d18ad6f7c71103caa1fab8e016994
Hamid <asheikh@aliviaanalytics.com>
2020-05-04 12:01:46 +0500

    context-menu

--- src/app/modules/base/common-components/grids/generic-grid/generic-grid.component.ts
+++ src/app/modules/base/common-components/grids/generic-grid/generic-grid.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-generic-grid',
   templateUrl: './generic-grid.component.html',
   styleUrls: ['./generic-grid.component.scss']
 })
-export class GenericGridComponent implements OnInit {
+export class GenericGridComponent implements OnInit, OnChanges, AfterViewInit {
 
   dtOptions: any = {};
-	@Input() data: any;
-	@Input() cols: any;
-	@Input() response: any;
+  @Input() data: any;
+  @Input() cols: any;
+  @Input() response: any;
 
   constructor() { }
 
@@ -18,11 +18,25 @@ export class GenericGridComponent implements OnInit {
   }
 
   ngOnChanges() {
-		if (this.response) {
-			this.setDtOptions();
-		}
+    if (this.response) {
+      this.setDtOptions();
+    }
   }
-  
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      $("#dtable tbody tr").bind("contextmenu", function (e) {        
+        e.preventDefault();
+        $("#box").css({ left: e.pageX, top: e.pageY });
+        $('#box').show();
+      });
+    }, 1000);
+      
+    $(document).bind("click", function (e) {
+      $('#box').hide();
+    });
+  }
+
   setDtOptions() {
     this.dtOptions = {
       paging: false,
