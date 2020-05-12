25813faf6cec8b7d8913d385257888dfe52ea3f9
Hamid <asheikh@aliviaanalytics.com>
2020-05-12 13:21:46 +0500

    models folder added

---
+++ src/app/models/RandomSampleDataModel.ts
@@ -0,0 +1,17 @@
+/**
+ * @author: Yashfa Iftikhar
+ * @since: 05-05-2020
+ */
+export class RandomSampleDataModel{
+	ai_workflow_primary_subj_claims_stage_3_standard_deviation: any;
+	ai_workflow_primary_subj_claims_stage_3_mean_total_claims_amount: any;
+	ai_workflow_primary_subj_claims_stage_3_total_claims: any;
+	ai_workflow_primary_subj_claims_stage3_no_of_claims: any;
+	ai_workflow_primary_subj_claims_stage_3_error_rate: any;
+	ai_workflow_primary_subj_claims_stage_3_seed_vaue: any;
+	ai_workflow_primary_subj_claims_stage_3_precision: any;
+	ai_workflow_primary_subj_claims_stage_3_confidence_level: any;
+	ai_workflow_primary_subj_claims_stage_3_ratstat_file_size: any;
+	ai_workflow_primary_subj_claims_stage_3_random_number_file: any;
+	ai_workflow_primary_subj_claims_stage_3_random_attachment: any;
+}
---
+++ src/app/models/complaint/primarySubModel.ts
@@ -0,0 +1,49 @@
+/**
+ * @author: Yashfa Iftikhar
+ * @since: 04-21-2020
+ */
+export class PrimarySubModel {
+	ai_workflow_pri_sub_id: any;
+	ai_workflow_pri_sub_type: any;
+	ai_workflow_pri_sub_id_type: any;
+	ai_workflow_pri_sub_taxonomy: any;
+	ai_workflow_pri_sub_auth_name:any;
+	ai_workflow_pri_sub_fname: any;
+	ai_workflow_pri_sub_lname: any;
+	ai_workflow_pri_sub_entity_name: any;
+	ai_workflow_pri_sub_address: any;
+	ai_workflow_pri_sub_city: any;
+	ai_workflow_pri_sub_state: any;
+	ai_workflow_pri_sub_zip: any;
+	ai_workflow_pri_sub_phone_number: any;
+	ai_workflow_pri_sub_email: any;
+	ai_workflow_pri_sub_notes: any;
+	ai_workflow_pri_sub_suspect: any;
+
+	ai_workflow_pri_sub_deactivation_reason: any;
+	ai_workflow_pri_sub_gender: any;
+	ai_workflow_pri_sub_entity_auth_mname: any;
+	ai_workflow_invp_primary_taxonomy_specialty: any;
+	ai_workflow_pri_sub_number_of_beds: any;
+	ai_workflow_pri_sub_reactivation_date: any;
+	ai_workflow_pri_sub_mailing_address_key: any;
+	ai_workflow_pri_sub_mname: any;
+	ai_workflow_pri_sub_parent_organization_name: any;
+	ai_workflow_pri_sub_mailing_address_second_line: any;
+	ai_workflow_pri_sub_employer_identification_number: any;
+	ai_workflow_pri_sub_pic_url: any;
+	ai_workflow_pri_sub_replacement_npi: any;
+	ai_workflow_pri_sub_primary_credential: any;
+	ai_workflow_pri_sub_primary_taxonomy_group: any;
+	ai_workflow_pri_sub_provider_name_prefix: any;
+	ai_workflow_pri_sub_primary_taxonomy_class: any;
+	ai_workflow_pri_sub_deactication_date: any;
+	ai_workflow_pri_sub_auth_fname: any;
+	ai_workflow_pri_sub_authorized_official_lname: any;
+	ai_workflow_pri_sub_parent_organization_tin: any;
+	ai_workflow_pri_sub_is_sole_proprietor: any;
+	ai_workflow_pri_sub_auth_lname: any;
+	ai_workflow_pri_sub_primary_language: any;
+	ai_workflow_pri_sub_name: any;
+	ai_workflow_pri_sub_rep_relation_to_sub: any;
+}
